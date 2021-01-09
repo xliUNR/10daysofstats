4e119f5ce29be07fd1fef2304315a8fdf82d0121
Xiang (Eric) Li <35667290+xliUNR@users.noreply.github.com>
2021-01-09 13:57:14 -0800

    Created statslib.py

---
+++ statslib.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+# This contains the stats library that I wrote for the exercises
+# imports
+import sys
+
+# median function 
+# Data must be sorted
+def median(sorted_data, start, end):
+    m = int((end + start) / 2)
+    
+    if (end - start ) % 2 == 0:
+        return int((sorted_data[m] + sorted_data[m-1]) / 2)
+    else:
+        return sorted_data[m]
+    
+#Quartile function that returns a tuple of the three quartiles        
+def quartiles(data, size):
+    Q1 = Q2 = Q3 = 0
+    m = int(size / 2)
+    # First data must be sorted
+    data.sort();
+
+    # calculate Q2
+    Q2 = median(data, 0, size)
+
+    # calculate quartiles using the median function depending on if size of data is even or odd
+    if size % 2 == 0:
+        Q1 = median(data, 0, m)
+        Q3 = median(data, m, size)
+    else:
+        Q1 = median(data, 0, m)
+        Q3 = median(data, m+1, size)    
+    
+    return (Q1, Q2, Q3)
+
+# returns the interquartile range        
+def interquartile_range(data, size):
+      Q1, Q2, Q3 = quartiles(data, size)
+      return float(Q3 - Q1)
+        
+
